c8b7448248fdc34d295ef3609eb128e08ac2a90a
digwijaybpatil <digwijaybpatil@gmail.com>
2025-11-28 18:36:44 +0530

    update todo-ui image version to v4 and switch API URLs to HTTPS

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // src/config.js
 
 const config = {
-  GET_TASKS_API_BASE_URL: "http://get.digwi.online",
-  DELETE_TASK_API_BASE_URL: "http://delete.digwi.online",
-  CREATE_TASK_API_BASE_URL: "http://add.digwi.online",
+  GET_TASKS_API_BASE_URL: "https://get.digwi.online",
+  DELETE_TASK_API_BASE_URL: "https://delete.digwi.online",
+  CREATE_TASK_API_BASE_URL: "https://add.digwi.online",
 };
 
 export default config;
