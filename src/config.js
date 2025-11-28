6f638ca83f16701ba4fdb7ec20674673fd70b782
digwijaybpatil <digwijaybpatil@gmail.com>
2025-11-28 17:54:46 +0530

    update todo-ui image version to v3 and restore API endpoint configurations

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 // src/config.js
 
 const config = {
-    GET_TASKS_API_BASE_URL: process.env.REACT_APP_GET_TASKS_API_BASE_URL || 'http://default-get-tasks-url',
-    DELETE_TASK_API_BASE_URL: process.env.REACT_APP_DELETE_TASK_API_BASE_URL || 'http://default-delete-task-url',
-    CREATE_TASK_API_BASE_URL: process.env.REACT_APP_CREATE_TASK_API_BASE_URL || 'http://default-create-task-url',
-  };
-  
-  export default config;
-  
+  GET_TASKS_API_BASE_URL: "http://get.digwi.online",
+  DELETE_TASK_API_BASE_URL: "http://delete.digwi.online",
+  CREATE_TASK_API_BASE_URL: "http://add.digwi.online",
+};
+
+export default config;
+
